858acf3931944529f43e241da18bcf8a20ffbbca
Paul Cheng <chengyuejia@foxmail.com>
2024-09-14 17:06:32 +0800

    feat: add scripts of install snarkjs2

--- scripts/local_compile.sh
+++ scripts/local_compile.sh
@@ -1,7 +1,18 @@
 
-# install related packages
-npm install -g circom@latest
-npm install
+# install snarjs2 which hash with blake3
+if type snarkjs2 >/dev/null; then
+    echo "snarkjs2 is installed (as a command, alias, or function)."
+else
+    git clone https://github.com/SuccinctPaul/snarkjs-blake3
+    cd snarkjs-blake3
+    # install related packages
+    npm install
+    npm install circom@latest
 
-# link related package
-npm link
+    # link related package
+    npm link
+    snarkjs2 -v
+    echo "snarkjs2 is installed (as a command, alias, or function)."
+fi
+
+
